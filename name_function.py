1c866674eb34f6dec9bc2d61664dff83d78cabce
potasioK <83794087+potasioK@users.noreply.github.com>
2021-07-19 02:45:38 -0400

    Add files via upload

---
+++ name_function.py
@@ -0,0 +1,10 @@
+def get_formatted_name(first, last, middle=''):
+    """Generates a neatly formatted name."""
+    if middle:
+        full_name = f"{first} {middle} {last}"
+    else:
+        full_name = f"{first} {last}"
+    return full_name.title()
+
+
+
